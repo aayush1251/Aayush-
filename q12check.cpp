bebc8a5b6675eafea66b242f482bb6f5c999c66e
aayush1251 <125898459+aayush1251@users.noreply.github.com>
2023-02-20 21:25:38 +0530

    Add files via upload

---
+++ q12check.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int isPalindrome(char *str, int n)
+{
+ 
+ int start_index = 0;
+ 
+ 
+ int last_index = n-1;
+ 
+ while(start_index < last_index && str[start_index] == str[last_index])
+ {
+ 
+  start_index++;
+  last_index--;
+ }
+ 
+ if(start_index < last_index)
+ {
+  
+  cout<<"NOT A PALINDROME"<<endl;
+  return 0;
+ }
+ else
+ {
+  
+  cout<<"PALINDROME"<<endl;
+  return 1;
+ }
+}
+
+int main(void)
+{
+ char *s1 = "atsaXaba";
+ char *s2 = "basaxnt";
+ char *s3 = "abababababbabXbababbbbabba";
+ 
+ int x;
+ x = isPalindrome(s1,7);
+ x = isPalindrome(s2,13);
+ x = isPalindrome(s3,27);
+ return 0;
+}
